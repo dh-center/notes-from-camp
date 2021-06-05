fe67d63a0fed932d54a621909b70e88bd90abc69
Nikita Melnikov <nikmel2803@gmail.com>
2021-06-05 17:16:03 +0300

    initial commit

---
+++ static/JS/app.js
@@ -0,0 +1,187 @@
+console.log("JS connected");
+
+function sub5_1(id) {
+    elm = document.getElementById(id);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 0.9;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub7_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 1;
+        secElm.style.opacity = 0.0;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub7_1_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    elm.style.opacity = 0.0;
+    secElm.style.opacity = 0.0;
+}
+
+function box7_1_out(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 0.0;
+}
+
+function box7_1_over(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 1.0;
+}
+
+function sub8_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 1;
+        secElm.style.opacity = 0.0;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub8_1_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    elm.style.opacity = 0.0;
+    secElm.style.opacity = 0.0;
+}
+
+function box8_1_out(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 0.0;
+}
+
+function box8_1_over(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 1.0;
+}
+
+function sub9_1(id, secId, thirdId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    thirdElm = document.getElementById(thirdId);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 1;
+        secElm.style.opacity = 0.0;
+        thirdElm.style.opacity = 0.0;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub9_1_1(id, secId, thirdId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    thirdElm = document.getElementById(thirdId);
+    elm.style.opacity = 0.0;
+    secElm.style.opacity = 0.0;
+    thirdElm.style.opacity = 0.0;
+}
+
+function box9_1_out(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 0.0;
+}
+
+function box9_1_over(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 1.0;
+}
+
+function sub10_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 1;
+        secElm.style.opacity = 0.0;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub10_1_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    elm.style.opacity = 0.0;
+    secElm.style.opacity = 0.0;
+}
+
+function box10_1_out(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 0.0;
+}
+
+function box10_1_over(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 1.0;
+}
+
+function sub11_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 1;
+        secElm.style.opacity = 0.0;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub11_1_1(id, secId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    elm.style.opacity = 0.0;
+    secElm.style.opacity = 0.0;
+}
+
+function box11_1_out(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 0.0;
+}
+
+function box11_1_over(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 1.0;
+}
+
+function sub12_1(id, secId, thirdId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    thirdElm = document.getElementById(thirdId);
+    if (elm.style.opacity == 0.0) {
+        elm.style.opacity = 1;
+        secElm.style.opacity = 0.0;
+        thirdElm.style.opacity = 0.0;
+    } else {
+        elm.style.opacity = 0.0;
+    }
+}
+
+function sub12_1_1(id, secId, thirdId) {
+    elm = document.getElementById(id);
+    secElm = document.getElementById(secId);
+    thirdElm = document.getElementById(thirdId);
+    elm.style.opacity = 0.0;
+    secElm.style.opacity = 0.0;
+    thirdElm.style.opacity = 0.0;
+}
+
+function box12_1_out(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 0.0;
+}
+
+function box12_1_over(id) {
+    elm = document.getElementById(id);
+    elm.style.opacity = 1.0;
+}
+
